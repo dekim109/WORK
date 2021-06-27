7430bf12d51b10828a19a1268dfeb23f2856efff
dekim109 <86557917+dekim109@users.noreply.github.com>
2021-06-27 21:51:30 +0900

    Add files via upload

---
+++ LoadingScene_test/LoadingSceneController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadingSceneController : MonoBehaviour
+{
+    static string nextScene;
+
+    [SerializeField]
+    Image progressBar;
+
+    public static void LoadScene(string sceneName)
+    {
+        nextScene = sceneName;
+        SceneManager.LoadScene("LoadingScene");
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadSceneProcess());
+    }
+
+    IEnumerator LoadSceneProcess()
+    {
+        AsyncOperation op = SceneManager.LoadSceneAsync(nextScene);
+        op.allowSceneActivation = false; //로딩 잠시 멈춤(fake loading) -> 생각보다 씬 로딩이 빠를 수 있음, asset 번들로 빌드하는 경우 - 리소스 로딩도 함께 필요
+
+        float timer = 0f;
+        while (!op.isDone)
+        {
+            yield return null;
+
+            if(op.progress < 0.9f)
+            {
+                progressBar.fillAmount = op.progress;
+            }
+            else
+            {
+                timer += Time.unscaledDeltaTime;
+                progressBar.fillAmount = Mathf.Lerp(0.9f, 1f, timer);
+                if(progressBar.fillAmount >= 1f)
+                {
+                    op.allowSceneActivation = true;
+                    yield break;
+                }
+            }
+        }
+    }
+}
